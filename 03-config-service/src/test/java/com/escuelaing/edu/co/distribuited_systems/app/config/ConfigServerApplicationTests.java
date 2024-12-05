34788ea1c0d84a1b87354fd460c8abfd78eb7b33
Ivan Camilo Rincon <ivan.rincon-s@mail.escuelaing.edu.co>
2024-12-04 21:41:46 -0500

    fixing pdf and adding config service

---
+++ 03-config-service/src/test/java/com/escuelaing/edu/co/distribuited_systems/app/config/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.escuelaing.edu.co.distribuited_systems.app.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
